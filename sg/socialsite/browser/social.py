4a31a703eae883595eadb6d1655c913035eea25a
Joaquín Rosales <globojorro@gmail.com>
2011-07-12 16:14:41 -0500

    Added Like/Dislike and Twitter boxes per content on social_summary_view

--- sg/socialsite/browser/social.py
+++ sg/socialsite/browser/social.py
@@ -1,3 +1,15 @@
 # -*- coding: utf-8 -*-
-from zope.interface import Interface
+# Zope version does not
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
+class SocialSummaryView(BrowserView):
+
+    # This may be overridden in ZCML
+    index = ViewPageTemplateFile("social_summary_view.pt")
+
+    def render(self):
+        return self.index()
+
+    def __call__(self):
+        return self.render()
