89a60db4d1f722c1babe19e516c228e7738cc230
Joaquín Rosales <globojorro@gmail.com>
2011-08-11 00:01:47 -0500

    Adding ILoveThumbsDontYou interface via subscribers for news items, questions, and code snippets

--- sg/socialsite/subscribers.py
+++ sg/socialsite/subscribers.py
@@ -1,16 +1,17 @@
-from five import grok
 from zope import event
+from zope.interface import alsoProvides
 from zope.app.container.interfaces import IObjectAddedEvent
 
 from Products.ATContentTypes.interfaces.news import IATNewsItem
 from cioppino.twothumbs.interfaces import ILoveThumbsDontYou
+from cioppino.twothumbs import rate
 from sg.questions.question import IQuestion
 from sg.pastebin.paste import IPaste
 
 
-@grok.subscribe(IATNewsItem, IObjectAddedEvent)
-def newsThumbs(news, event):
-    if not ILoveThumbsDontYou.providedBy(news):
-        alsoProvides(news, ILoveThumbsDontYou)
-        self.news.reindexObject()
+def contextThumbs(context, event):
+    if not ILoveThumbsDontYou.providedBy(context):
+        alsoProvides(context, ILoveThumbsDontYou)
+        rate.setupAnnotations(context)
+        context.reindexObject()
 
