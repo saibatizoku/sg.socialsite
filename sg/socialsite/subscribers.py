27385e22643564ee28c453d588878a2f71f50bff
Joaquín Rosales <globojorro@gmail.com>
2011-08-11 00:03:36 -0500

    Cleaned up subscribers.py file

--- sg/socialsite/subscribers.py
+++ sg/socialsite/subscribers.py
@@ -1,12 +1,7 @@
-from zope import event
 from zope.interface import alsoProvides
-from zope.app.container.interfaces import IObjectAddedEvent
 
-from Products.ATContentTypes.interfaces.news import IATNewsItem
 from cioppino.twothumbs.interfaces import ILoveThumbsDontYou
 from cioppino.twothumbs import rate
-from sg.questions.question import IQuestion
-from sg.pastebin.paste import IPaste
 
 
 def contextThumbs(context, event):
